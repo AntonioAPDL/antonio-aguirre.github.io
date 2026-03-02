a4fd83e1997bbe1091c4b87fa6a660de49c68d75
Antonio <antonio.agprz@gmail.com>
2026-03-01 21:07:54 -0800

    Reduce observed discharge line thickness

--- public/js/sanlorenzo_flow.js
+++ public/js/sanlorenzo_flow.js
@@ -480,7 +480,7 @@
       name: 'Observed',
       showlegend: true,
       legendrank: 10,
-      line: { color: colors.line, width: 2.6 },
+      line: { color: colors.line, width: 1.8 },
       marker: {
         color: colors.line,
         size: 2.8,
